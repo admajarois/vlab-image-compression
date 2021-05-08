53cf28782c1e3c370a259c2d8e6fc9a262c0f7b7
AdmajaRois <rois.admj@gmail.com>
2021-05-08 22:22:10 +0700

    new page for login and register

---
+++ app/image_compression.py
@@ -0,0 +1,60 @@
+import cv2
+import numpy as np
+import json
+
+from app import functions
+
+def image_compression(image):
+    original_image = cv2.imread(image)
+    original_image = cv2.cvtColor(original_image, cv2.COLOR_BGR2RGB)
+    image_height = original_image.shape[0]
+    image_width = original_image.shape[1]
+
+    red_scale_image = original_image[:,:,0]
+    green_scale_image = original_image[:,:,1]
+    blue_scale_image = original_image[:,:,2]
+
+    red_channel_histogram_array = functions.histogram_generator.histogram_array_generator(red_scale_image)
+    green_channel_histogram_array = functions.histogram_generator.histogram_array_generator(green_scale_image)
+    blue_channel_histogram_array = functions.histogram_generator.histogram_array_generator(blue_scale_image)
+
+    red_channel_probability_distribution = functions.histogram_generator.probability_distribution_generator(red_channel_histogram_array, image_height*image_width)
+    green_channel_probability_distribution = functions.histogram_generator.probability_distribution_generator(green_channel_histogram_array, image_height*image_width)
+    blue_channel_probability_distribution = functions.histogram_generator.probability_distribution_generator(blue_channel_histogram_array, image_height*image_width)
+
+    red_channel_probability_distribution['seperator'] = 0
+    red_huffman_coding = functions.huffman_coding.Huffman_Coding(red_channel_probability_distribution)
+    red_coded_pixels, red_reverse_coded_pixels = red_huffman_coding.compress()
+
+    green_channel_probability_distribution['seperator'] = 0
+    green_huffman_coding = functions.huffman_coding.Huffman_Coding(green_channel_probability_distribution)
+    green_coded_pixels, green_reverse_coded_pixels = green_huffman_coding.compress()
+
+    blue_huffman_coding = functions.huffman_coding.Huffman_Coding(blue_channel_probability_distribution)
+    blue_coded_pixels, blue_reverse_coded_pixels = blue_huffman_coding.compress()
+
+    with open('static/codes/red_channel_codes.json', 'w') as fp:
+        json.dump(red_coded_pixels,fp)
+    with open('static/decodes/red_channel_decodes.json', 'w') as fp:
+        json.dump(red_reverse_coded_pixels,fp)
+
+    with open('static/codes/green_channel_codes.json', 'w') as fp:
+        json.dump(green_coded_pixels,fp)
+    with open('static/decodes/green_channel_decodes.json', 'w') as fp:
+        json.dump(green_reverse_coded_pixels,fp)
+
+    with open('static/codes/blue_channel_codes.json', 'w') as fp:
+        json.dump(blue_coded_pixels,fp)
+    with open('static/decodes/blue_channel_decodes.json', 'w') as fp:
+        json.dump(blue_reverse_coded_pixels,fp)
+
+    red_channel_compressed_image = functions.image_compressor.compressor(red_scale_image, red_coded_pixels)
+    green_channel_compressed_image = functions.image_compressor.compressor(green_scale_image, green_coded_pixels)
+    blue_channel_compressed_image = functions.image_compressor.compressor(blue_scale_image, blue_coded_pixels)
+
+    bit_stream = byte_stream_generator.byte_stream(red_channel_compressed_image, green_channel_compressed_image, blue_channel_compressed_image, red_coded_pixels['seperator'], green_coded_pixels['seperator'])
+
+    print('Compression ratio:', (len(bit_stream)/(red_scale_image.shape[0]*red_scale_image.shape[1]*3*8)))
+
+    with open('static/bit_stream.txt', 'w') as fp:
+        fp.write(bit_stream)
--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,7 @@
 from app import app
 from flask import render_template, redirect, request, url_for
 from werkzeug.utils import secure_filename
+from app import image_compression
 
 @app.route('/')
 @app.route('/index')
@@ -8,17 +9,23 @@ def index():
 
     greeting = 'Hello Dunia!'
 
-    return render_template('index.html', title='Hello', greeting=greeting)
+    return render_template('index.html', title='Home', greeting=greeting)
 
 @app.route('/materi')
 def materi():
 
     return render_template('materi.html', materi=materi)
 
+@app.route('/pendahuluan')
+def pendahuluan():
+   judul = "Pendahuluan"
+
+   return render_template('pendahuluan.html', title=judul, judul=judul)
+
 @app.route('/eksperimen')
 def eksperimen():
 
-    return render_template('eksperimen.html', eksperimen=eksperimen)
+    return render_template('eksperimen.html',title="Program Kompresi", eksperimen=eksperimen)
 
 @app.route('/compress')
 def compress():
@@ -27,11 +34,24 @@ def compress():
 
     return render_template('compress.html',title='compressing', compress=compress)
 
-@app.route('/compress', methods=['POST'])
-def upload_image():
-    uploaded_image = request.files['file']
-    if uploaded_image.filename != '':
-        uploaded_image.save(uploaded_image.filename)
-    return redirect(url_for('compress'))
+@app.route('/proses')
+def proses():
+
+    image = url_for('static', filename='img/stones.jpg')
+
+    image_compression.image_compression(image)
+
+    return url_for('compress')
 
 
+@app.route('/login')
+def login():
+    greeting = "Selamat datang!"
+
+    return render_template('login.html', title='Login', greeting=greeting)
+
+@app.route('/register')
+def register():
+    greeting = "Daftar!"
+
+    return render_template('register.html', title='Login', greeting=greeting)
